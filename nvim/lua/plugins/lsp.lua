8892f4a1cbf23c7de870e413cfe2faa59dc8d3ce
Andy Wang <andywang0321@gmail.com>
2025-05-15 01:03:13 -0700

    oh ok so I need to go disable a bunch of LSPs in Mason
    ran `:LspInfo` and discovered that a bunch of legacy LSPs are running at
    the same time

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -133,38 +133,38 @@ return { -- LSP Configuration & Plugins
                     },
                 },
             },
-            pylsp = {
-                settings = {
-                    pylsp = {
-                        plugins = {
-                            pyflakes = { enabled = false },
-                            pycodestyle = { enabled = false },
-                            autopep8 = { enabled = false },
-                            yapf = { enabled = false },
-                            mccabe = { enabled = false },
-                            pylsp_mypy = { enabled = false },
-                            pylsp_black = { enabled = false },
-                            pylsp_isort = { enabled = false },
-                        },
-                    },
-                },
-            },
-            -- basedpyright = {
-            --     -- Config options: https://github.com/DetachHead/basedpyright/blob/main/docs/settings.md
+            -- pylsp = {
             --     settings = {
-            --         basedpyright = {
-            --             disableOrganizeImports = true, -- Using Ruff's import organizer
-            --             disableLanguageServices = false,
-            --             analysis = {
-            --                 ignore = { '*' }, -- Ignore all files for analysis to exclusively use Ruff for linting
-            --                 typeCheckingMode = 'off',
-            --                 diagnosticMode = 'openFilesOnly', -- Only analyze open files
-            --                 useLibraryCodeForTypes = true,
-            --                 autoImportCompletions = true, -- whether pyright offers auto-import completions
+            --         pylsp = {
+            --             plugins = {
+            --                 pyflakes = { enabled = false },
+            --                 pycodestyle = { enabled = false },
+            --                 autopep8 = { enabled = false },
+            --                 yapf = { enabled = false },
+            --                 mccabe = { enabled = false },
+            --                 pylsp_mypy = { enabled = false },
+            --                 pylsp_black = { enabled = false },
+            --                 pylsp_isort = { enabled = false },
             --             },
             --         },
             --     },
             -- },
+            basedpyright = {
+                -- Config options: https://github.com/DetachHead/basedpyright/blob/main/docs/settings.md
+                settings = {
+                    basedpyright = {
+                        disableOrganizeImports = true, -- Using Ruff's import organizer
+                        disableLanguageServices = false,
+                        analysis = {
+                            ignore = { '*' }, -- Ignore all files for analysis to exclusively use Ruff for linting
+                            typeCheckingMode = 'off',
+                            diagnosticMode = 'openFilesOnly', -- Only analyze open files
+                            useLibraryCodeForTypes = true,
+                            autoImportCompletions = true, -- whether pyright offers auto-import completions
+                        },
+                    },
+                },
+            },
             ruff = {
                 -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
                 -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
