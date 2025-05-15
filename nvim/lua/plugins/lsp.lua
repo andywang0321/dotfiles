e8bb89414a6368835fe736f4b6c6966b870ae715
Andy Wang <andywang0321@gmail.com>
2025-05-14 23:32:56 -0700

    tried basedpyright for a bit, gave up, maybe come back another day

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -150,20 +150,20 @@ return { -- LSP Configuration & Plugins
                 },
             },
             -- basedpyright = {
-            --   -- Config options: https://github.com/DetachHead/basedpyright/blob/main/docs/settings.md
-            --   settings = {
-            --     basedpyright = {
-            --       disableOrganizeImports = true, -- Using Ruff's import organizer
-            --       disableLanguageServices = false,
-            --       analysis = {
-            --         ignore = { '*' },                 -- Ignore all files for analysis to exclusively use Ruff for linting
-            --         typeCheckingMode = 'off',
-            --         diagnosticMode = 'openFilesOnly', -- Only analyze open files
-            --         useLibraryCodeForTypes = true,
-            --         autoImportCompletions = true,     -- whether pyright offers auto-import completions
-            --       },
+            --     -- Config options: https://github.com/DetachHead/basedpyright/blob/main/docs/settings.md
+            --     settings = {
+            --         basedpyright = {
+            --             disableOrganizeImports = true, -- Using Ruff's import organizer
+            --             disableLanguageServices = false,
+            --             analysis = {
+            --                 ignore = { '*' }, -- Ignore all files for analysis to exclusively use Ruff for linting
+            --                 typeCheckingMode = 'off',
+            --                 diagnosticMode = 'openFilesOnly', -- Only analyze open files
+            --                 useLibraryCodeForTypes = true,
+            --                 autoImportCompletions = true, -- whether pyright offers auto-import completions
+            --             },
+            --         },
             --     },
-            --   },
             -- },
             ruff = {
                 -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
